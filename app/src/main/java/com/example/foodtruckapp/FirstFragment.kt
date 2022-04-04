2d186ed4ed6d421805af32ef27a8c0bba4d15c1d
itberaahim <raahim.owais7@gmail.com>
2022-04-03 22:40:14 -0500

    set up MapsActivity.kt and button to start

--- app/src/main/java/com/example/foodtruckapp/FirstFragment.kt
+++ app/src/main/java/com/example/foodtruckapp/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.example.foodtruckapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -26,7 +27,6 @@ class FirstFragment : Fragment() {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,6 +34,11 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        binding.mapButton.setOnClickListener{
+            val mapIntent = Intent(activity, MapsActivity::class.java)
+            startActivity(mapIntent)
         }
     }
 
