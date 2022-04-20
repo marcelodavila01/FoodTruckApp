68f3889c190db4d970c1027d15627b42550f57fe
Marcelo Davila <marcelodavilagonzalez@gmail.com>
2022-04-19 20:44:21 -0500

    Fixed layout for Owner sign up activity, Customer sign up activity and Location activity so that all objects are viewable in both a Pixel 2 and Pixel 3 XL emulators - Marcelo

--- app/src/main/java/com/example/foodtruckapp/FifthItemAdapter/FifthItemAdapter.kt
+++ app/src/main/java/com/example/foodtruckapp/FifthItemAdapter/FifthItemAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodtruckapp.R
 import com.example.foodtruckapp.FifthModel.FifthFragment
--- app/src/main/java/com/example/foodtruckapp/FifthModel/FifthFragment.kt
+++ app/src/main/java/com/example/foodtruckapp/FifthModel/FifthFragment.kt
@@ -1,6 +1,7 @@
 package com.example.foodtruckapp.FifthModel
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.fragment.app.Fragment
 import androidx.annotation.DrawableRes
 import android.view.LayoutInflater
@@ -8,8 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
+import com.example.foodtruckapp.R
+
 //import com.example.foodtruckapp.databinding.FragmentFifthBinding
 
-data class FifthFragment(val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
+data class FifthFragment(val stringResourceId: Int, @DrawableRes val imageResourceId: Int){
 
 }
